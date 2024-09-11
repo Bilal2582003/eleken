5e6529142484e0c118129ac8af4ba104a8eb7586
Muhammad Bilal <huzaifa2582003@gmail.com>
2024-09-11 16:17:58 +0500

    data

--- connection.php
+++ connection.php
@@ -1,16 +1,16 @@
 <?php
 
 
-$database="eleken";
-$username="root";
-$password="";
-$hostname="localhost";
-
-
-// $database="cyberns2_eleken";
-// $username="cyberns2_saimagroup";
-// $password="03172746242dA";
+// $database="eleken";
+// $username="root";
+// $password="";
 // $hostname="localhost";
+
+
+$database="cyberns2_eleken";
+$username="cyberns2_saimagroup";
+$password="03172746242dA";
+$hostname="localhost";
 
 // $conn= mysqli_connect($hostname,$username,$password,$database) or die("Connection failed");
 
